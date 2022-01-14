33f04ff182924ebd822da5a2625db0bd4ebb378d
Priyansh Saini <sainip9211@gmail.com>
2022-01-14 22:15:55 +0530

    implementing friendly id on posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  resources :technologies
-  get 'about-developer', to: 'pages#about'
-  get 'contact', to: 'pages#contact' 
-  resources :posts
+  resources :technologies, except: [:show]
+  get 'technology/:id', to: 'technologies#show', as: 'technology_show'
 
-  root to: "pages#home" 
+  get 'about-developer', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  resources :posts
+  get 'post/:id', to: 'posts#show'
+
+  root to: "pages#home"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_11_193322) do
+ActiveRecord::Schema.define(version: 2022_01_14_163119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
 
   create_table "functionalities", force: :cascade do |t|
     t.string "title"
@@ -28,6 +39,8 @@ ActiveRecord::Schema.define(version: 2022_01_11_193322) do
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
   create_table "technologies", force: :cascade do |t|
