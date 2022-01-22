3b6c8f456149b397a7f70309026dc45bb3351521
Priyansh Saini <sainip9211@gmail.com>
2022-01-22 17:42:47 +0530

    post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy toggle_status]
   layout "post"
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index], site_admin: {except: [:destroy, :new, :create, :update, :edit, 
+  :toggle_status]}, user: :all
 
 # one other option that might seem a bit weird is to put a group of roles in an array:
 access [:all, :user] => [:show, :index]
