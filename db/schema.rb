ef34400a5ee69619f0e0996640e584277693cc2b
Priyansh Saini <sainip9211@gmail.com>
2022-01-25 19:26:44 +0530

    added position column to technology

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_21_112832) do
+ActiveRecord::Schema.define(version: 2022_01_25_055519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2022_01_21_112832) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status", default: 0
+    t.integer "position"
   end
 
   create_table "users", force: :cascade do |t|
